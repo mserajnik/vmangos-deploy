94386e53d2d8ac645b9f69e401664e1e062b89ee
Michael Serajnik <m@mser.at>
2024-06-11 11:42:16 +0200

    Update MariaDB entrypoint script check

--- docker/database/docker-entrypoint.sh
+++ docker/database/docker-entrypoint.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # This entrypoint script is based on
-# https://github.com/MariaDB/mariadb-docker/blob/4e20774a56c8fb93cec9ee9d4a5b476bc0f8dd0d/11.4/docker-entrypoint.sh
+# https://github.com/MariaDB/mariadb-docker/blob/81c4ce79659a9b705686b5704a3f3d3b9119532a/11.4/docker-entrypoint.sh
 # and might need to get adjusted when the original script gets updated.
 # Formatting, comments and commented out code from the original script have
 # been preserved, where possible, to make it easier to compare this script to
