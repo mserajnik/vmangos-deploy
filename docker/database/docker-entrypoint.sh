d7fa200d1d16fda8e59674a45df2c279aeed8912
Michael Serajnik <m@mser.at>
2025-08-10 10:41:49 +0200

    Upgrade database base image to MariaDB 11.8 LTS

--- docker/database/docker-entrypoint.sh
+++ docker/database/docker-entrypoint.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # This entrypoint script is based on
-# https://github.com/MariaDB/mariadb-docker/blob/a272347802e1764dd8c0e15ba2b2abfeeadb3bb6/11.4/docker-entrypoint.sh
+# https://github.com/MariaDB/mariadb-docker/blob/cdffb7d2fd712249f3f386497117825be6442afa/11.8/docker-entrypoint.sh
 # and might need to get adjusted when the original script gets updated.
 # Formatting, comments and commented out code from the original script have
 # been preserved, where possible, to make it easier to compare this script to
